a3105e09a8228d5541989ddc4d9387d88274e5cb
Vitaly <vitaly1090@mail.ru>
2024-12-22 23:58:07 +1000

    sessionFactory

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -1,13 +1,17 @@
 package com.dmdev;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateRunner {
 
-    public static void main(String[] args) throws SQLException {
-        Connection connection = DriverManager
-                .getConnection("jdbc:postgresql://localhost:5433/postgres", "postgres", "pass");
+    public static void main(String[] args) {
+        Configuration configuration = new Configuration();
+        configuration.configure();
+        try (SessionFactory sessionFactory = configuration.buildSessionFactory();
+             Session session = sessionFactory.openSession()) {
+            System.out.println("OK");
+        }
     }
 }
