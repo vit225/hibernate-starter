ab761f16e8e40b4fc1f15aa3c3d0066eca0fa46d
Vitaly <vitaly1090@mail.ru>
2024-12-23 12:33:18 +1000

    entity and session

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -1,8 +1,11 @@
 package com.dmdev;
 
+import com.dmdev.entity.User;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.time.LocalDate;
 
 public class HibernateRunner {
 
@@ -11,7 +14,18 @@ public class HibernateRunner {
         configuration.configure();
         try (SessionFactory sessionFactory = configuration.buildSessionFactory();
              Session session = sessionFactory.openSession()) {
-            System.out.println("OK");
+            session.beginTransaction();
+
+            User user = User.builder()
+                    .username("ivan@gmail.com")
+                    .firstname("Ivan")
+                    .lastname("Ivanov")
+                    .birthDate(LocalDate.of(2000, 1, 19))
+                    .age(20)
+                    .build();
+            session.save(user);
+
+            session.getTransaction().commit();
         }
     }
 }
