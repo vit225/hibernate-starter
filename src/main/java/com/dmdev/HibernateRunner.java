616fbe37e2ac81da1d6b9f24d1ee677f0d95101c
Vitaly <vitaly1090@mail.ru>
2024-12-23 20:19:33 +1000

    update delete get

--- src/main/java/com/dmdev/HibernateRunner.java
+++ src/main/java/com/dmdev/HibernateRunner.java
@@ -1,15 +1,11 @@
 package com.dmdev;
 
 import com.dmdev.converter.BirthdayConverter;
-import com.dmdev.entity.Birthday;
-import com.dmdev.entity.Role;
 import com.dmdev.entity.User;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
-
-import java.time.LocalDate;
 
 public class HibernateRunner {
 
@@ -22,20 +18,20 @@ public class HibernateRunner {
              Session session = sessionFactory.openSession()) {
             session.beginTransaction();
 
-            User user = User.builder()
-                    .username("ivan2@gmail.com")
-                    .firstname("Ivan")
-                    .lastname("Ivanov")
-                    .info("""
-                            {
-                                "name": "Ivan",
-                                "id": 25
-                            }
-                            """)
-                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
-                    .role(Role.ADMIN)
-                    .build();
-            session.save(user);
+//            User user = User.builder()
+//                    .username("ivan9@gmail.com")
+//                    .firstname("Ivan")
+//                    .lastname("Ivanov")
+//                    .info("""
+//                            {
+//                                "name": "Ivan",
+//                                "id": 25
+//                            }
+//                            """)
+//                    .birthDate(new Birthday(LocalDate.of(2000, 1, 19)))
+//                    .role(Role.ADMIN)
+//                    .build();
+            User user = session.get(User.class, "ivan@gmail.com");
 
             session.getTransaction().commit();
         }
